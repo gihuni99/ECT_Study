f4ae59de34e53e16f0d3900ff6013f99b13c0497
Gihun Son <thlg60@naver.com>
2023-03-03 00:02:45 +0900

    Library(math)

---
+++ Chapter01/Basic문법/6. 주요 라이브러리의 문법과 유의점/Math.py
@@ -0,0 +1,15 @@
+#math
+#자주 사용되는 수학적인 기능을 포함하는 라이브러리
+import math
+
+#factorial
+print(math.factorial(5)) #5!을 구할 수 있다.
+
+#sqrt(제곱근)
+print(math.sqrt(7)) #7의 제곱근 출력
+
+#gcd(최대 공약수)
+print(math.gcd(21,14)) # 21과 14의 최대 공약수
+
+print(math.pi) # 파이
+print(math.e) # 자연상수 e
