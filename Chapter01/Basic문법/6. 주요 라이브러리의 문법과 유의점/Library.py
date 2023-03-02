f6fc5aa2f5bd5a6f90c2915280d5f2a8fb0dd696
Gihun Son <thlg60@naver.com>
2023-03-02 22:10:51 +0900

    라이브러리 설명(Library)

---
+++ Chapter01/Basic문법/6. 주요 라이브러리의 문법과 유의점/Library.py
@@ -0,0 +1,11 @@
+#표준 라이브러리란?
+#특정한 프로그래밍 언어에서 자주 사용되는 표준 소스코드를 미리 구현해 놓은 라이브러리
+#https://docs.python.org/ko/3/library/index.html
+#위 사이트는 파이썬 라이브러리를 찾을 수 있는 사이트
+#가장 중요한 6가지 라이브러리
+#1. 내장 함수
+#2. itertools
+#3. heapq
+#4. bisect
+#5. collections
+#6. math
